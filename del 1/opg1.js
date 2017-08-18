80ca1e63b52466b02421bd66be057692617b7fcb
CamillaKurup <4342295@RTS-365.dk>
2017-08-18 09:22:54 +0200

    aflevering

---
+++ del 1/opg1.js
@@ -0,0 +1,6 @@
+var forste = 3;
+
+do{
+    console.log ("hej");
+    forste ++;
+}while(forste<8)
---
+++ del 1/opg2.js
@@ -0,0 +1,14 @@
+var i = 1;
+
+while(i<26){
+    console.log ("test" + i);
+    i ++;
+}
+i= 1;
+do{
+    console.log ("hej" + i);
+    i ++;
+}while (i<26);
+
+for(var i  = 1; i < 26; i++)
+    console.log ("fisk" + i)
